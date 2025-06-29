9c41286da155adf429136a810e78d6e9fd872ae1
LatikaMadankar <latika9511@gmail.com>
2025-06-29 18:25:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# New Project
+
+This project is a web dev project using HTML CSS JavaScript.
---
+++ app.js
@@ -0,0 +1,134 @@
+let inventory = [];
+
+let form = document.querySelector("#addItem-form");
+let tableBody = document.querySelector("#tableBody");
+let totalItemDisplay = document.querySelector(".item-count");
+let lowstockDisplay = document.querySelector(".low-stock-count");
+let searchInput = document.querySelector("#searchInput");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let name = document.querySelector("#item-name").value.trim();
+    let category = document.querySelector("#item-category").value.trim();
+    let quantity = parseInt(document.querySelector("#item-quantity").value);
+
+    inventory.push({name, category, quantity});
+    //row created
+    let tr = document.createElement("tr");
+
+    let status = quantity < 10 ? "Low" : "Sufficient";
+    let statusClass = quantity < 10 ? "low" : "sufficient";
+
+    tr.innerHTML = `
+        <td>${name}</td>
+        <td>${category}</td>
+        <td>${quantity}</td>
+        <td>
+        <div class="status-wrapper">
+            <span class="status ${statusClass}">${status}</span>
+                <img src="delete.png" alt="Delete" class="delete-icon" title="Delete">
+            </div>
+        </td>
+    `;
+    //delete button
+    let deleteBtn = tr.querySelector(".delete-icon");
+    deleteBtn.addEventListener("click", () => {
+        tr.remove();
+        let index = inventory.findIndex(item => item.name === name && item.category === category && item.quantity === quantity);
+        if (index !== -1) inventory.splice(index, 1);
+        updateStats();
+        saveToStorage();
+    });
+
+    tableBody.appendChild(tr);
+    updateStats();
+    saveToStorage();
+    form.reset();
+});
+
+function updateStats() {
+    totalItemDisplay.textContent = inventory.length;
+    let lowCount = inventory.filter(item => item.quantity <10).length;
+    lowstockDisplay.textContent = lowCount;
+}
+
+function saveToStorage() {
+    localStorage.setItem("inventoryData", JSON.stringify(inventory));
+}
+
+function loadFromStorage() {
+    inventory = [];
+    tableBody.innerHTML = "";
+    
+    let data = localStorage.getItem("inventoryData");
+    if (data) {
+        const savedInventory = JSON.parse(data);
+        savedInventory.forEach(item => {
+            inventory.push(item);
+            addItemToTable(item.name, item.category, item.quantity);
+        });
+        
+    }else {
+  
+    let defaultItems = [
+      { name: "ERW Steel Tubes", category: "Welded Tubes", quantity: 120 },
+      { name: "Cold Pilgered Seamless Tubes", category: "Precision Tubing", quantity: 45 },
+      { name: "Automotive Exhaust Pipes", category: "Automotive Components", quantity: 15 },
+      { name: "Boiler Tubes", category: "Industrial Tubing", quantity: 70 }
+    ];
+
+    defaultItems.forEach(item => {
+      inventory.push(item);
+      addItemToTable(item.name, item.category, item.quantity);
+    });
+
+    saveToStorage();
+  }   
+  updateStats();
+}    
+
+searchInput.addEventListener("keyup", () => {
+    let filter = searchInput.value.toLowerCase();
+    let rows = tableBody.querySelectorAll("tr");
+
+    rows.forEach(row=>{
+        let itemName = row.cells[0].textContent.toLowerCase();
+        let itemCategory = row.cells[1].textContent.toLowerCase();
+        let match = itemName.includes(filter) || itemCategory.includes(filter);
+        row.style.display = match ? "" : "none";
+    });
+});
+
+
+function addItemToTable(name, category, quantity) {
+    let tr = document.createElement("tr");
+
+    let status = quantity < 10 ? "Low" : "Sufficient";
+    let statusClass = quantity < 10 ? "low" : "sufficient";
+
+    tr.innerHTML = `
+        <td>${name}</td>
+        <td>${category}</td>
+        <td>${quantity}</td>
+        <td>
+            <div class="status-wrapper">
+                <span class="status ${statusClass}">${status}</span>
+                <img src="delete.png" alt="Delete" class="delete-icon" title="Delete">
+            </div>
+        </td>
+    `;
+
+    let deleteBtn = tr.querySelector(".delete-icon");
+    deleteBtn.addEventListener("click", () => {
+        tr.remove();
+        let index = inventory.findIndex(item => item.name === name && item.category === category && item.quantity === quantity);
+        if (index !== -1) inventory.splice(index, 1);
+        updateStats(); 
+        saveToStorage();
+    });
+
+    tableBody.appendChild(tr);
+}
+
+loadFromStorage();
